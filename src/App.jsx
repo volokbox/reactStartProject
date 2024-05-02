dec3872185bbb792b3e4d4b9c3189ef2b629072c
Volodymyr Chernikov <volodymyr3574@gmail.com>
2024-05-02 10:43:01 +0100

    Added random React description, optimized image path

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function getRandomInt(max){
+  return Math.floor(Math.random() * (max+1));
+}
+
+function Header(){
+  const description = reactDescription[getRandomInt(2)]; 
+
+  return(
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header/>
       <main>
         <h2>Time to get started!</h2>
       </main>
