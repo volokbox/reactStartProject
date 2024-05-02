3f4c4a02263330485c6bc63adec40a70786d1dc1
Volodymyr Chernikov <volodymyr3574@gmail.com>
2024-05-02 13:21:15 +0100

    Finished interactive examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES, CORE_CONCEPTS } from './data-with-examples.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [ selectedTopic, setSelectedTopic] = useState('Please click the button');
+  const [ selectedTopic, setSelectedTopic] = useState('components');
 
   function handleClick(selectedButton){
     setSelectedTopic(selectedButton);
@@ -34,8 +34,14 @@ function App() {
             <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
             <TabButton onSelect={() => handleClick('state')}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
-        {selectedTopic}
       </main>
     </div>
   );
